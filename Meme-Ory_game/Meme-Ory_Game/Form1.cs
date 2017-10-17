0901503843bb40e1d2f3cb5e82f84cceef374fe3
DanielvdH <hove1500@student.nhl.nl>
2017-10-17 14:32:56 +0200

    Fixed EVERYTHING -kinda-

--- Meme-Ory_game/Meme-Ory_Game/Form1.cs
+++ Meme-Ory_game/Meme-Ory_Game/Form1.cs
@@ -17,28 +17,37 @@ namespace Meme_Ory_Game
         Button Start = new Button();
         Button Secondstart = new Button();
         TableLayoutPanel MemoryPanel = new TableLayoutPanel();
+        Button firstClicked = null;
+        Button secondClicked = null;
+        bool player1beurt = true;
+        int player1score = 0;
+        int player2score = 0;
+        Label Player1Score = new Label();
+        Label Player2Score = new Label();
+        TextBox PlayerName_1 = new TextBox();
+        TextBox PlayerName_2 = new TextBox();
+        Label PlayerNameLabel_1 = new Label();
+        Label PlayerNameLabel_2 = new Label();
+        Label PlayerTurn = new Label();
+        int maxscore = 0;
 
         public Form1()
         {
             InitializeComponent();
-            CreateStartbutton();           
+            CreateStartbutton();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         public void CreateStartbutton()
         {
-            int topstart = 200;
-            int leftstart = 200;
             this.Controls.Add(Start);
-            Start.Size = new Size(150, 150);
-            Start.Left = leftstart;
-            Start.Top = topstart;
+            Start.Location = new Point(100, 150);
+            Start.Size = new Size(200, 50);
             Start.Text = "start";
-            Start.BackgroundImageLayout = ImageLayout.Stretch;
             Start.Click += new EventHandler(this.Startbutton_Click);
         }
 
@@ -46,14 +55,15 @@ namespace Meme_Ory_Game
         {
             this.Controls.Add(Secondstart);
             Secondstart.Size = new Size(100, 100);
-            Secondstart.Location= new Point (100,100);
+            Secondstart.Location = new Point(100, 250);
+            Secondstart.Text = "Start Game";
             Secondstart.Click += new EventHandler(this.Secondstart_Click);
         }
 
         public void CreateComboBox()
         {
             this.Controls.Add(Speelveld);
-            Speelveld.Location = new Point (100, 200);
+            Speelveld.Location = new Point(100, 200);
             Speelveld.Size = new Size(100, 100);
             Speelveld.Items.AddRange(new object[] { "4x4", "5x5", "6x6", "7x7", "8x8" });
         }
@@ -64,7 +74,7 @@ namespace Meme_Ory_Game
             int leftreset = 100;
             Button reset = new Button();
             this.Controls.Add(reset);
-            reset.Size = new Size(100,20);
+            reset.Size = new Size(100, 20);
             reset.Left = leftreset;
             reset.Top = topreset;
             reset.Text = "reset";
@@ -78,18 +88,18 @@ namespace Meme_Ory_Game
             MemoryPanel.RowStyles.Clear();
             MemoryPanel.ColumnStyles.Clear();
             int Amountx = Convert.ToInt16(Math.Pow(Amount, 2));
-            int[] numberCards = new int[Amountx];       
+            int[] numberCards = new int[Amountx];
             int[] randomarray = random(numberCards);
-            MemoryPanel.Location = new System.Drawing.Point(26, 12);
-            MemoryPanel.Size = new System.Drawing.Size(300,300);
+            MemoryPanel.Location = new System.Drawing.Point(200, 10);
+            MemoryPanel.Size = new System.Drawing.Size(300, 300);
             MemoryPanel.RowCount = Amount;
             MemoryPanel.ColumnCount = Amount;
-            for (int i = 0; i < (Amount); i++)        
+            for (int i = 0; i < (Amount); i++)
             {
                 MemoryPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10));
                 MemoryPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
             }
-            for (int i = 0; i < (Amount * Amount); i++)            
+            for (int i = 0; i < (Amount * Amount); i++)
             {
                 var button = new Button();
                 button.Text = Convert.ToString(randomarray[i]);
@@ -98,32 +108,73 @@ namespace Meme_Ory_Game
                 MemoryPanel.Controls.Add(button);
                 button.Click += new System.EventHandler(FlipCard);
             }
+            maxscore = Amount * Amount / 2;
             this.Controls.Add(MemoryPanel);
+        }
+
+        public void CreatePlayerName()
+        {
+            this.Controls.Add(PlayerName_1);
+            this.Controls.Add(PlayerName_2);
+            PlayerName_1.Location = new Point(100, 50);
+            PlayerName_2.Location = new Point(100, 100);
+            PlayerName_1.Size = new Size(100, 50);
+            PlayerName_2.Size = new Size(100, 50);
+            PlayerName_1.Text = PlayerTurn.Text;
+            PlayerName_2.Text = PlayerTurn.Text;
+        }
+
+        public void CreatePlayerNameLabel()
+        {
+            this.Controls.Add(PlayerNameLabel_1);
+            this.Controls.Add(PlayerNameLabel_2);
+            PlayerNameLabel_1.Location = new Point(10, 10);
+            PlayerNameLabel_2.Location = new Point(10, 60);
+            PlayerNameLabel_1.Size = new Size(100, 50);
+            PlayerNameLabel_2.Size = new Size(100, 50);
+            PlayerNameLabel_1.Text = PlayerName_1.Text;
+            PlayerNameLabel_2.Text = PlayerName_2.Text;
+        }
+
+        public void CreatePlayerScore()
+        {
+            this.Controls.Add(Player1Score);
+            this.Controls.Add(Player2Score);
+            this.Controls.Add(PlayerTurn);
+            Player1Score.Text = Convert.ToString(player1score);
+            Player2Score.Text = Convert.ToString(player2score);
+            Player1Score.Location = new Point(110, 10);
+            Player2Score.Location = new Point(110, 60);
+            PlayerTurn.Location = new Point(30, 120);
+            Player1Score.Size = new Size(30,30);
+            Player2Score.Size = new Size(30, 30);
         }
 
         public void Startbutton_Click(object sender, EventArgs e)
         {
             CreateSecondStart();
             CreateComboBox();
+            CreatePlayerName();
             Start.Visible = false;
         }
 
-        public void Secondstart_Click (object sender, EventArgs e)
+        public void Secondstart_Click(object sender, EventArgs e)
         {
             if (Speelveld.Text == "4x4")
                 Amount = 4;
-            if (Speelveld.Text == "5x5")
-                Amount = 5;
             if (Speelveld.Text == "6x6")
                 Amount = 6;
-            if (Speelveld.Text == "7x7")
-                Amount = 7;
             if (Speelveld.Text == "8x8")
                 Amount = 8;
             Speelveld.Visible = false;
-            Secondstart.Visible = false;
+            Start.Visible = false;
+            PlayerName_1.Visible = false;
+            PlayerName_2.Visible = false;
+            PlayerTurn.Text = PlayerName_1.Text + "'s turn";
+            CreateLayoutPanel();
+            CreatePlayerNameLabel();
+            CreatePlayerScore();
             CreateResetButton();
-            CreateLayoutPanel();                    
         }
 
         public void Resetbutton_Click(object sender, EventArgs e)
@@ -132,20 +183,85 @@ namespace Meme_Ory_Game
         }
 
         public static int[] random(int[] array)         // Method to refill the 0,0 array with random non-duplicate numbers between from 1 to 16
-            {
-                var random = new Random();
+        {
+            var random = new Random();
 
-                int[] randomArray = Enumerable.Range(1, array.Length).OrderBy(x => random.Next()).ToArray();
-                return randomArray;
+            int[] randomArray = Enumerable.Range(1, array.Length).OrderBy(x => random.Next()).ToArray();
+            return randomArray;
+        }
+
+        public async void FlipCard(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            int nummer = Convert.ToInt32(clickedButton.Name);
+            clickedButton.Image = KrijgImage(nummer); //nummer = naam van button = het nummer uit de random array.
+
+            if (firstClicked == null)// filling 1st comparer
+            {
+                firstClicked = clickedButton;
+                return;
             }
 
-        private void FlipCard(object sender, EventArgs e)
-        {
-                Button clickedButton = sender as Button;
-                int nummer = Convert.ToInt32(clickedButton.Name);
-                clickedButton.BackgroundImage = KrijgImage(nummer); //nummer = naam van button = het nummer uit de random array.
-                clickedButton.BackgroundImageLayout = ImageLayout.Stretch;
-        }
+            secondClicked = clickedButton; // filling 2nd comparer   
+
+            if ((Convert.ToInt32(firstClicked.Text) % 2 != 0))
+            {
+                if (Convert.ToInt32(firstClicked.Text) == (Convert.ToInt32(secondClicked.Text) - 1))
+                {
+                    if (player1beurt == true) { player1score++; Player1Score.Text = Convert.ToString(player1score); }
+                    else { player2score++; Player1Score.Text = Convert.ToString(player2score); }
+
+                    if (player1score + player2score == maxscore - 1) { MessageBox.Show("biatch"); }
+                    firstClicked.Enabled = false;
+                    secondClicked.Enabled = false;
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+                else
+                {
+                    if (player1beurt == true) { player1beurt = false; PlayerTurn.Text = PlayerName_2.Text + "'s turn"; }
+                    else { player1beurt = true; PlayerTurn.Text = PlayerName_1.Text + "'s turn"; }
+                    await Task.Delay(500);
+                    firstClicked.Image = Properties.Resources.image_1;
+                    secondClicked.Image = Properties.Resources.image_1;
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+            }
+            else
+            {
+                if (Convert.ToInt32(firstClicked.Text) == (Convert.ToInt32(secondClicked.Text) + 1))
+                {
+                    if (player1beurt == true)
+                    {
+                        player1score++; Player1Score.Text = Convert.ToString(player1score);
+                    }
+                    else
+                    {
+                        player2score++; Player2Score.Text = Convert.ToString(player2score);
+                    }
+                    if (player1score + player2score == maxscore - 1) { MessageBox.Show("biatch"); }
+                    firstClicked.Enabled = false;
+                    firstClicked.Enabled = false;
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+                else
+                {
+                    if (player1beurt == true) { player1beurt = false; PlayerTurn.Text = PlayerName_2.Text + "'s turn"; }
+                    else { player1beurt = true; PlayerTurn.Text = PlayerName_1.Text + "'s turn"; }
+                    await Task.Delay(500);
+                    firstClicked.Image = Properties.Resources.image_1;
+                    secondClicked.Image = Properties.Resources.image_1;
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+            }
+        } 
 
         public static Image KrijgImage(int nummer) //Method voor de BackgroundImage
         {
